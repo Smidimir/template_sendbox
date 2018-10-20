e3d4beb3e0c3d6ee0ff3abf6918646a8b1c5ace4
smidimir <smidimir@gmail.ocm>
2018-10-21 02:29:14 +0300

    update: value to type convertor v2

--- main.cpp
+++ main.cpp
@@ -164,100 +164,225 @@ std::array<int, 3> constexpr constexpr_text()
 
 
 /**
- * value to type mapping
+ * ConstructInformer
  */
 
-#include <map>
 
-template <typename T>
-struct type_wrapper {};
-
-template <typename Variant>
-struct VariantToTypeWrapper;
-
-template <typename ... Ts>
-struct VariantToTypeWrapper<std::variant<Ts ...>>
-{
-  using type = std::variant<type_wrapper<Ts> ...>;
-};
-
-template <typename ... Ts>
-struct TypeList {};
-
-template <typename Variant>
-struct VariantToTypeList;
-
-template <typename ... Ts>
-struct VariantToTypeList<std::variant<Ts ...>>
-{
-  using type = TypeList<Ts...>;
-};
-
-template <typename ValType, typename Variant>
-class ValToTypeMap
+class ConstructInformer
 {
 public:
-  using TWVariant = typename VariantToTypeWrapper<Variant>::type;
-  using TList = typename VariantToTypeList<Variant>::type;
-
-  template <typename ... Ts>
-  ValToTypeMap(Ts&& ... ts)
+  ConstructInformer()
   {
-    addType(TList{}, std::forward<Ts>(ts) ...);
+    std::cout << "ConstructInformer: default" << std::endl;
   }
 
-  Variant create(ValType const& val)
+  ConstructInformer(ConstructInformer const&)
   {
-    return std::visit([this](auto const& v) { return create(v);}, m_map[val]);
+    std::cout << "ConstructInformer: copy" << std::endl;
   }
+
+  ConstructInformer(ConstructInformer&&)
+  {
+    std::cout << "ConstructInformer: move" << std::endl;
+  }
+
+  ConstructInformer& operator = (ConstructInformer const&)
+  {
+    std::cout << "ConstructInformer: assign" << std::endl;
+    return *this;
+  }
+
+  ConstructInformer& operator = (ConstructInformer&&)
+  {
+    std::cout << "ConstructInformer: move assign" << std::endl;
+    return *this;
+  }
+};
+
+
+/**
+ * value to type mapping v2
+ */
+
+#include <variant>
+#include <type_traits>
+#include <map>
+#include <utility>
+#include <tuple>
+
+
+template <typename T, typename ... Ts>
+struct type_list
+{
+  using first = T;
+  using rest = type_list<Ts ...>;
+
+  static std::size_t constexpr size = sizeof ... (Ts) + 1u;
+};
+
+template <typename T1, typename T2>
+struct type_list<T1, T2>
+{
+  using first = T1;
+  using rest = T2;
+
+  static std::size_t constexpr size = 2u;
+};
+
+template <typename T>
+struct type_list<T>
+{
+  using first = T;
+
+  static std::size_t constexpr size = 1u;
+};
+
+template <typename T>
+using type_holder = type_list<T>;
+
+template <typename ... Ts>
+using type_list_ft = typename type_list<Ts ...>::first;
+
+template <typename ... Ts>
+using type_list_rt = typename type_list<Ts ...>::rest;
+
+template <typename ... Ts>
+auto constexpr type_list_sv = type_list<Ts ...>::size;
+
+
+// >>>>
+// >>>>>
+// >>>>
+
+
+#include <cassert>
+
+template <typename T>
+class variant_object_creator;
+
+template <typename ... Types>
+class variant_object_creator<std::variant<Types ...>>
+{
+public:
+  using variant_type = std::variant<type_holder<Types> ...>;
+  using variant_object_type = std::variant<Types ...>;
+
+  constexpr variant_object_creator(variant_object_creator const&) = default;
+  constexpr variant_object_creator(variant_object_creator&&) noexcept = default;
+
+  template <typename T>
+  explicit constexpr variant_object_creator(T&& t)
+    : m_cur_type {std::forward<T>(t)}
+  {;}
+
+
+  variant_object_creator& operator = (variant_object_creator const&) = default;
+  variant_object_creator& operator = (variant_object_creator&&) noexcept = default;
+
+  template <typename T>
+  variant_object_creator& operator = (type_holder<T> const& t)
+  {
+    m_cur_type = t;
+  }
+
+  template <typename T>
+  variant_object_creator& operator = (type_holder<T>&& t)
+  {
+    m_cur_type = std::move(t);
+  }
+
+  variant_object_creator& operator = (variant_type const& t)
+  {
+    m_cur_type = t;
+  }
+
+  variant_object_creator& operator = (variant_type&& t)
+  {
+    m_cur_type = std::move(t);
+  }
+
+public:
 
   template <typename ... Args>
-  Variant create(ValType const& val, Args&& ... args)
+  variant_object_type operator() (Args&& ... args)
   {
-    return std::visit([this, args = std::make_tuple(std::forward<Args>(args) ...)](auto const& v) { return create(v, std::move(args));}, m_map[val]);
+    return std::visit([this, t_args = std::forward_as_tuple(args ...)](auto&& t) -> variant_object_type
+    {
+      return construct_from_tuple(t, t_args, std::make_index_sequence<std::tuple_size<decltype(t_args)>{}>{});
+    }, m_cur_type);
   }
 
-//private:
-  template <typename T>
-  Variant create(type_wrapper<T>)
+private:
+  template <typename T, typename Tuple, size_t ... Indeces>
+  variant_object_type construct_from_tuple(type_holder<T> t, Tuple&& tuple, std::index_sequence<Indeces ...>)
   {
-    return T{};
+    return construct(t, std::forward<std::tuple_element_t<Indeces, std::decay_t<Tuple>>>(std::get<Indeces>(tuple))...);
   }
 
-  template <typename T, typename Tuple>
-  Variant create(type_wrapper<T>, Tuple&& t)
+  template <typename T, typename ... Args>
+  variant_object_type construct(type_holder<T>, Args&& ... args)
   {
-    return create(type_wrapper<T>{}, std::forward<Tuple>(t),
-        std::make_index_sequence<std::tuple_size<Tuple>::value>() );
+    if constexpr (std::is_constructible_v<T, decltype(std::forward<Args>(args)) ...>)
+    {
+      return T{std::forward<Args>(args) ...};
+    }
+    else
+    {
+      assert(false);
+      return T{};
+    }
   }
 
-  template <typename T, typename Tuple, std::size_t... Inds>
-  Variant create(type_wrapper<T>, Tuple&& tuple, std::index_sequence<Inds...>)
+private:
+  variant_type m_cur_type;
+};
+
+// >>>>
+// >>>>>
+// >>>>
+
+
+template <typename KeyType, typename VariantType>
+class vt_mapper;
+
+template <typename KeyType, typename ... Types>
+class vt_mapper<KeyType, std::variant<Types ...>>
+{
+public:
+  using variant_type = std::variant<type_holder<Types> ...>;
+  using variant_object_type = std::variant<Types ...>;
+
+  template <typename ... Ts>
+  explicit vt_mapper(Ts&& ... keys)
   {
-    return T(std::get<Inds>(std::forward<Tuple>(tuple))...);
+    (m_map.insert_or_assign(std::forward<Ts>(keys), variant_type{type_holder<Types>{}}), ...);
   }
 
-  template <typename T, typename Arg, typename ... Ts, typename ... Args>
-  void addType(TypeList<T, Ts ...>, Arg&& arg, Args&& ... args)
+  variant_object_creator<variant_object_type> operator[] (KeyType&& key)
   {
-    m_map[arg] = type_wrapper<T>{};
-    addType(TypeList<Ts ...>{}, std::forward<Args>(args) ...);
+    return variant_object_creator<variant_object_type>{m_map.at(std::forward<KeyType>(key))};
   }
 
-  template <typename T, typename Arg>
-  void addType(TypeList<T> typeList, Arg&& arg)
-  {
-    m_map[arg] = type_wrapper<T>{};
-  }
-
-//private:
-
-  std::map<ValType, TWVariant> m_map;
+private:
+  std::map<KeyType, variant_type> m_map;
 };
 
 
 int main()
 {
+  using V = std::variant<char, int, long, ConstructInformer>;
+
+  vt_mapper<int, V> mapper{1, 2, 3, 4};
+
+  variant_object_creator<V> voc{type_holder<ConstructInformer>{}};
+
+  V v1 = mapper[4](ConstructInformer{});
+  V v2 = mapper[4](std::move(ConstructInformer{}));
+
+
+//  std::visit([](auto&& v) { std::cout << typeid(v).name() << std::endl; }, v1);
+
+
 //  auto constexpr arr = constexpr_text();
 //
 //  std::array<int, arr[0]> arr1;
@@ -266,25 +391,27 @@ int main()
 //
 //  std::cout << arr1.size() << " " << arr2.size() << " " << arr3.size() << std::endl;
 
-  using V = std::variant<int, char, double>;
+//  using V = std::variant<int, char, double, ConstructInformer>;
 
-  ValToTypeMap<int, V> mapper{1, 2, 3};
+//  ValToTypeMap<int, V> mapper{1, 2, 3, 4};
 
-  std::cout << mapper.m_map.size() << std::endl;
+//  std::cout << mapper.m_map.size() << std::endl;
+//
+//  std::visit([](auto const& v){ std::cout << typeid(v).name() << std::endl; }, mapper.m_map[1]);
+//  std::visit([](auto const& v){ std::cout << typeid(v).name() << std::endl; }, mapper.m_map[2]);
+//  std::visit([](auto const& v){ std::cout << typeid(v).name() << std::endl; }, mapper.m_map[3]);
+//  std::visit([](auto const& v){ std::cout << typeid(v).name() << std::endl; }, mapper.m_map[4]);
 
-  std::visit([](auto const& v){ std::cout << typeid(v).name() << std::endl; }, mapper.m_map[1]);
-  std::visit([](auto const& v){ std::cout << typeid(v).name() << std::endl; }, mapper.m_map[2]);
-  std::visit([](auto const& v){ std::cout << typeid(v).name() << std::endl; }, mapper.m_map[3]);
+//  auto a = mapper.create(1);
+//  auto b = mapper.create(1);
+//  auto c = mapper.create(2);
+//  auto d = mapper.create(3, 'a');
+//  auto e = mapper.create(4, ConstructInformer{});
 
-  auto a = mapper.create(1);
-  auto b = mapper.create(1, 'a');
-  auto c = mapper.create(2, 'a');
-  auto d = mapper.create(3, 'a');
-
-  std::visit([](auto const& v){ std::cout << typeid(v).name() << " : " << v << std::endl; }, a);
-  std::visit([](auto const& v){ std::cout << typeid(v).name() << " : " << v << std::endl; }, b);
-  std::visit([](auto const& v){ std::cout << typeid(v).name() << " : " << v << std::endl; }, c);
-  std::visit([](auto const& v){ std::cout << typeid(v).name() << " : " << v << std::endl; }, d);
+//  std::visit([](auto const& v){ std::cout << typeid(v).name() << " : " << v << std::endl; }, a);
+//  std::visit([](auto const& v){ std::cout << typeid(v).name() << " : " << v << std::endl; }, b);
+//  std::visit([](auto const& v){ std::cout << typeid(v).name() << " : " << v << std::endl; }, c);
+//  std::visit([](auto const& v){ std::cout << typeid(v).name() << " : " << v << std::endl; }, d);
 
   return 0;
 }
